77ef9d5a005305447154423d201713984a66bedb
希理 <1437565863@qq.com>
2019-02-20 12:11:47 -0500

    Unit 1 Activity 2 Assignment

--- Unit 1/Activity 2/morse_code.py
+++ Unit 1/Activity 2/morse_code.py
@@ -33,6 +33,45 @@ def input_choose():
     return choose
 
 def main():
+    '''
+    variable require: None(type)
+
+    function require:
+        input_choose
+        encrypt_or_decrypt
+        main
+
+    purpose: this is the main function of the problem
+
+    return variable: None
+    '''
+
+    print('This program will encrypt or decrypt a phrase using the simple encryption method of rotating the letters')
+    phrase = input('Please enter a phrase: ').lower()
+    choose = input_choose()
+    try:
+        encrypt_or_decrypt(phrase,choose)
+    except:
+        print()
+        print('error: make sure you enter the right code/word and choose the right option')
+        print('If there is any pronlem, please contact me')
+        print()
+        main()
+
+def encrypt_or_decrypt(phrase,choose):
+    '''
+    variable require: 
+        phrase(str,only letter or only '.'and'-')
+        choose(int, 1 or 2 )# choose==1 : encrypt
+
+
+    function require: None
+
+    purpose: this function will encrypt or decrypt morse cide
+
+    return variable: None
+    '''
+
     chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
     codes = """.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-..
     -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..
@@ -41,11 +80,27 @@ def main():
     char_to_code = dict(zip(chars.lower(),codes.split()))
     code_to_char = dict(zip(codes.split(),chars.lower()))
 
-    print('This program will encrypt or decrypt a phrase using the simple encryption method of rotating the letters')
-    phrase = input('Please enter a phrase: ').upper()
-    choose = input_choose()
+    if choose == 1:
+        code_list = []
+        print('The original phrase is:',phrase)
+        for letter in phrase:
+            print(char_to_code[letter])
+            code_list.append(char_to_code[letter])
+        print()
+        print('The encrypted phrase is:',end=' ')
+        for code in code_list:
+            print(code,end=' ')
+    elif choose == 2:
+        letter_list = []
+        print('The encrypted code is:',phrase)
+        for letter in phrase.split(' '):
+            print(code_to_char[letter])
+            letter_list.append(code_to_char[letter])
+        print()
+        print('The decrypted letters is',end=' ')
+        for letter in letter_list:
+            print(letter,end='')
 
-def encrypt_or_decrypt(phrase,)
     
 
 if __name__ == "__main__":
