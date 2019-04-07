32498a471204400634f51efd53438887e16db4c6
希理 <35692076+YuudachiYuudachiPoi@users.noreply.github.com>
2019-04-07 16:08:29 -0400

    Unit4 Activity2

--- Unit 4/Activity 2/sorting_routines.py
+++ Unit 4/Activity 2/sorting_routines.py
@@ -5,7 +5,7 @@ Author: Howie Hong(希理)
 LastEditors: Howie Hong(希理)
 Description: 
 Date: 2019-04-07 12:44:35
-LastEditTime: 2019-04-07 15:04:35
+LastEditTime: 2019-04-07 16:07:24
 '''
 
 class NumbersList:
@@ -70,12 +70,70 @@ class NumbersList:
     def reverse(self):
         self.data = self.data[::-1]
 
-    def prt(self):
-        print(self.data)
+    def print(self):
+        for num in self.data:
+            print(num,end=' ')
+        print()
 
 def main():
-    
+    number_of_numbers = int(input('How many random numbers do you wish to generate? '))
+    number_list = NumbersList(number_of_numbers)
+    number_list.make_list()
 
+    type_of_sort = 0
+    while not type_of_sort in range(1,5):
+        type_of_sort = int(input('''
+        What type of sort would you like to perfrom?
+        1 - Selection Sort
+        2 - Bubble Sort
+        3 - Insertion Sort
+        4 - Quick Sort
+        '''))
+    print()
+
+    type_of_order = 0
+    while not type_of_order in range(1,3):
+        type_of_order = int(input('''
+        In what order do you want the numbers sorted?
+        1 - Ascending
+        2 - Descending
+        '''))
+    '''
+    description: 
+    param {type} 
+    return: 
+    '''
+    print()
+    
+    print('The unsorted list is:')
+    number_list.print()
+    print('-'*30)
+    message = ['Parfoming']
+    if type_of_sort == 1:
+        number_list.selection_sort()
+        message.append('Selection Sort')
+    elif type_of_sort == 2:
+        number_list.bubble_sort()
+        message.append('Bubble Sort')
+    elif type_of_sort == 3:
+        number_list.insertion_sort()
+        message.append('Insertion Sort')
+    elif type_of_sort == 4:
+        number_list.quick_sort()
+        message.append('Quick Sort')
+    
+    if type_of_order == 1:
+        message.insert(1,'Ascending')
+    elif type_of_order == 2:
+        number_list.reverse()
+        message.insert(1,'Descending')    
+
+    print(' '.join(message))
+    print('The sorted list is:')
+    number_list.print()
+
+if __name__ == "__main__":
+    main()
 
 '''
 a = NumbersList(10)
